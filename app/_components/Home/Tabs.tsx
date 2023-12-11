db910ae27d3ba7991ad6624e35e03986d41dff2f
omar-salama <omarzalama@gmail.com>
2023-12-11 19:16:09 +0200

    fix tabs on smaller window

--- app/_components/Home/Tabs.tsx
+++ app/_components/Home/Tabs.tsx
@@ -7,13 +7,13 @@ const RecipeList: React.FC<{ tabs: string[] }> = ({ tabs }) => {
   };
 
   return (
-    <ul className="flex">
+    <ul className="flex flex-wrap">
       {tabs.map((tab, index) => (
         <li
           key={index}
           className={`${
             activeTab === tab ? 'active-tab' : 'inactive-tab'
-          } ${index < tabs.length - 1 ? 'me-2' : ''} cursor-pointer`}
+          } ${index < tabs.length - 1 ? 'me-2' : ''} cursor-pointer mt-3 sm:mt-0`}
           onClick={() => handleTabClick(tab)}
         >
           {tab}
