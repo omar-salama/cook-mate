91df6fec37755b2cfe209d62340dac503352d64a
omar-salama <omarzalama@gmail.com>
2023-12-12 18:15:37 +0200

    fixed build error

--- app/api/recipes/[id]/route.ts
+++ app/api/recipes/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server"
 import prisma from '../../../../lib/prisma';
 
 export async function GET(
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   const post = await prisma.recipe.findUnique({
