a4980a79ce6984de3a087ce0b55e04aa54a2cb35
omar-salama <omarzalama@gmail.com>
2023-12-13 20:02:16 +0200

    show nav links based on current page

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
+  const isHomePath = pathname === '/';
+  const isAddPath = pathname === '/recipe/add';
   return (
     <div className='flex items-center justify-between mt-6 mb-12 sm:mb-14 sm:mt-16'>
       <Link href='/'>
@@ -14,15 +19,17 @@ export default function Navbar() {
       </Link>
       <ul className='flex flex-col sm:flex-row items-center text-sm font-semibold'>
         <li className='sm:mx-3 sm:my-3 my-1'>Recent Recipes</li>
-        <li className='sm:mx-3 sm:my-3 my-1'>Account</li>
-        <li className='ms-3 sm:mt-0 mt-3'>
-          <Link
-            className='bg-secondary hover:bg-secondary/[.9] text-white rounded-full py-3 px-4 sm:px-6'
-            href='/recipe/add'
-          >
-            Add New Recipe
-          </Link>
-        </li>
+        {isHomePath && <li className='sm:mx-3 sm:my-3 my-1'>Account</li>}
+        {!isAddPath && (
+          <li className='ms-3 sm:mt-0 mt-3'>
+            <Link
+              className='bg-secondary hover:bg-secondary/[.9] text-white rounded-full py-3 px-4 sm:px-6'
+              href='/recipe/add'
+            >
+              Add New Recipe
+            </Link>
+          </li>
+        )}
       </ul>
     </div>
   );
