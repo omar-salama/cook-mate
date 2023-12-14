7543dba6cde434d5133fed12c2da5cfac32b1e86
omar-salama <omarzalama@gmail.com>
2023-12-14 17:11:10 +0200

    remove navbar links on auth routes

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -7,6 +7,7 @@ export default function Navbar() {
   const pathname = usePathname();
   const isHomePath = pathname === '/';
   const isAddPath = pathname === '/recipe/add';
+  const isAuth = pathname === '/login' || pathname === '/register';
   return (
     <div className='flex items-center justify-between mt-6 mb-12 sm:mb-14 sm:mt-16'>
       <Link href='/'>
@@ -17,20 +18,22 @@ export default function Navbar() {
           height={42}
         />
       </Link>
-      <ul className='flex flex-col sm:flex-row items-center text-sm font-semibold'>
-        <li className='sm:mx-3 sm:my-3 my-1'>Recent Recipes</li>
-        {isHomePath && <li className='sm:mx-3 sm:my-3 my-1'>Account</li>}
-        {!isAddPath && (
-          <li className='ms-3 sm:mt-0 mt-3'>
-            <Link
-              className='bg-secondary hover:bg-secondary/[.9] text-white rounded-full py-3 px-4 sm:px-6'
-              href='/recipe/add'
-            >
-              Add New Recipe
-            </Link>
-          </li>
-        )}
-      </ul>
+      {!isAuth && (
+        <ul className='flex flex-col sm:flex-row items-center text-sm font-semibold'>
+          <li className='sm:mx-3 sm:my-3 my-1'>Recent Recipes</li>
+          {isHomePath && <li className='sm:mx-3 sm:my-3 my-1'>Account</li>}
+          {!isAddPath && (
+            <li className='ms-3 sm:mt-0 mt-3'>
+              <Link
+                className='bg-secondary hover:bg-secondary/[.9] text-white rounded-full py-3 px-4 sm:px-6'
+                href='/recipe/add'
+              >
+                Add New Recipe
+              </Link>
+            </li>
+          )}
+        </ul>
+      )}
     </div>
   );
 }
