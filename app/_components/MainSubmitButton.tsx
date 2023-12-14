b01080acaf6318e2261e9bb78ad1fb9faae2b84e
omar-salama <omarzalama@gmail.com>
2023-12-14 18:38:34 +0200

    fix button on hover while submitting

--- app/_components/MainSubmitButton.tsx
+++ app/_components/MainSubmitButton.tsx
@@ -7,8 +7,8 @@ const MainSubmitButton = ({
 }) => {
   return (
     <button
-      className={`w-full rounded-full bg-secondary hover:bg-secondary/[.9] text-white font-bold py-2 ${
-        loading ? 'cursor-wait bg-secondary/[.6] hover:bg-secondary/[.6]' : ''
+      className={`w-full rounded-full text-white font-bold py-2 ${
+        loading ? 'cursor-wait bg-secondary/[.6] hover:bg-secondary/[.6]' : 'bg-secondary hover:bg-secondary/[.9]'
       }`}
       type='submit'
       disabled={loading}
